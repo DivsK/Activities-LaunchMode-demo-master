d7c726aef5b3e2801e6ab0a88579ccc43b216e8e
Faisal Khan <faisalkhan1690@gmail.com>
2015-12-16 18:17:22 +0530

    Initial commit

---
+++ app/src/main/java/com/novoda/demos/activitylaunchmode/SingleTask.java
@@ -0,0 +1,10 @@
+package com.novoda.demos.activitylaunchmode;
+
+public class SingleTask extends BaseActivity {
+
+    @Override
+    public int getBackgroundColour() {
+        return R.color.yellow;
+    }
+
+}
